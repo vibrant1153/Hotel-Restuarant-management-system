380333d8031caa7821e40478f3405fb1a174aaf9
vibrant1153 <bytheconsentofallah@gmail.com>
2026-05-13 23:12:28 +0300

    report/route.js updated

--- src/app/api/reports/route.js
+++ src/app/api/reports/route.js
@@ -2,3 +2,11 @@ import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
+
+export async function GET(){
+    try{
+        const session = await getServerSession(authOptions);
+    }catch{
+
+    }
+}
