c4a2a40e242782bc07480aa13bafb34625442e4c
vibrant1153 <bytheconsentofallah@gmail.com>
2026-05-13 23:13:27 +0300

    report/route.js updated

--- src/app/api/reports/route.js
+++ src/app/api/reports/route.js
@@ -6,6 +6,10 @@ import { authOptions } from '@/lib/auth';
 export async function GET(){
     try{
         const session = await getServerSession(authOptions);
+
+        if (!session || session.user.role !== 'ADMIN') {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
     }catch{
 
     }
